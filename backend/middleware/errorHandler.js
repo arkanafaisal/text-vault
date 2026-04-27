5874befebc4180ffa2b7bee35044dc9679751941
arkanafaisal <arkana.faisal.r@gmail.com>
2026-04-27 20:25:49 +0700

    change subdomain to textvault & reduce err message logging

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,10 +1,9 @@
 export function errorHandler(err, req, res, next){
-    console.log(err.message)
     let code = 500
 
     if(err.code === 'ER_DUP_ENTRY' || err.message === 'duplicate'){code = 409}
 
-
+    if(code !== 500){console.log(err.message)}
 
     return res.sendStatus(code)
 };
